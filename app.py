8e116ded6943ac3e12e67cef53e974088fa0af28
MingFei <mingfei2001@gmail.com>
2024-06-16 07:40:53 +0800

    added new files

--- app.py
+++ app.py
@@ -28,5 +28,13 @@ def home():
     conn.close()
     return render_template('index.html', plan=plan)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
