4d03fa09bae9f8f4cbd4b99bea2ae66d31e856c5
MingFei <mingfei2001@gmail.com>
2024-06-16 06:56:39 +0800

    commit

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, request
+# Importing the libraries
+from flask import Flask, render_template
 import sqlite3
 
 #create flask app
 app = Flask(__name__, static_folder='static')
 
-connection = sqlite3.connect('database.db')
+################################################################
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+# Creating the database for testing purposes
+# connection = sqlite3.connect('database.db')
+# with open('schema.sql') as f:
+    # connection.executescript(f.read())
+# connection.commit()
+# connection.close()
 
-connection.commit()
-connection.close()
+################################################################
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    conn = get_db_connection()
+    plan = conn.execute('SELECT * FROM plan').fetchall() 
+    conn.close()
+    return render_template('index.html', plan=plan)
 
 if __name__ == '__main__':
     app.run(debug=True)
