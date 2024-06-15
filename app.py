e1e3d6a2c868fc60f44b94b9d2e413b552de980a
MingFei <mingfei2001@gmail.com>
2024-06-16 06:27:57 +0800

    commits

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+#create flask app
+app = Flask(__name__, static_folder='static')
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
