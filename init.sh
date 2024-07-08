226e6a332c0f0b2e48934f7c186e78eb5620be7a
MingFei2001 <mingfei2001@gmail.com>
2024-07-08 09:43:56 +0800

    added gunicorn inside init script

--- init.sh
+++ init.sh
@@ -9,11 +9,12 @@ echo "| Initiating virtual environment ..."
 
 virtualenv pyenv
 source ./pyenv/bin/activate
-pip install flask flask_mysqldb sqlite3 MySQL-python gunicorn
+pip install flask flask_mysqldb gunicorn
 
 echo "| Starting service now ..."
 
 gunicorn -b localhost:8000 app-bk:app
+# gunicorn -b localhost:8000 app:app
 
 echo "| Service terminated"
 echo "| Program exiting ..."
