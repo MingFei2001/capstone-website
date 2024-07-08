2d58bf28d396808c35880b08399c78724d215471
MingFei2001 <mingfei2001@gmail.com>
2024-07-08 16:59:03 +0800

    turn gunicorn into background process upon launch

--- init.sh
+++ init.sh
@@ -10,11 +10,11 @@ echo "| Initiating virtual environment ..."
 
 virtualenv pyenv
 source ./pyenv/bin/activate
-pip install flask flask_mysqldb gunicorn
+pip install flask gunicorn
 
 echo "| Starting service now ..."
 
-gunicorn -b localhost:8000 app-bk:app
+gunicorn -b localhost:8000 app-bk:app &
 # gunicorn -b localhost:8000 app:app
 
 echo "| Service terminated"
