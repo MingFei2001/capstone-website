d8e1be1afc99f93640b7251b0b9062b8319f936a
MingFei2001 <mingfei2001@gmail.com>
2024-07-08 17:14:24 +0800

    added build step for mysqldb

--- init.sh
+++ init.sh
@@ -5,11 +5,16 @@ echo "| Installing dependencies ..."
 sudo apt-get update
 sudo apt-get install python3 python3-pip
 sudo apt-get install python3-virtualenv
+sudo apt-get install mysql-server
 
 echo "| Initiating virtual environment ..."
 
 virtualenv pyenv
 source ./pyenv/bin/activate
+
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential pkg-config
+pip install mysqlclient
+pip install flask_mysqldb
 pip install flask gunicorn
 
 echo "| Starting service now ..."
